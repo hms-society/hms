54e553efe7f21d0248f46c65db1dcd07ac2d0c7e
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-08-05 09:36:17 -0300

    refactor(server): remove unused intake list repository constant

--- apps/server/src/intake/constants/intake-list-repositories.ts
+++
@@ -1,3 +0,0 @@
-export const INTAKE_LIST_REPOSITORIES = {
-  intakeList: Symbol('INTAKE_LIST_REPOSITORIES.intakeList'),
-} as const
--- apps/server/src/intake/constants/intake-repositories.ts
+++ apps/server/src/intake/constants/intake-repositories.ts
@@ -1,7 +1,4 @@
 export const INTAKE_REPOSITORIES = {
   intakes: Symbol('INTAKE_REPOSITORIES.intakes'),
-<<<<<<< HEAD
-=======
   intakeList: Symbol('INTAKE_LIST_REPOSITORIES.intakeList'),
->>>>>>> feat/intakes-page
 } as const
