6fe128a55c0029431b7c215b831511a409b44c43
juliaprianti06 <juliaprianti3382@hotmail.com>
2026-08-24 18:04:28 -0300

    chore: remove unused file

--- packages/core/src/document-engine/domain/errors/document-acess-denied-error.ts
+++
@@ -1,6 +0,0 @@
-export class DocumentAccessDeniedError extends Error {
-  constructor(message: string = 'Você não tem permissão para acessar este documento.') {
-    super(message)
-    this.name = 'DocumentAccessDeniedError'
-  }
-}
